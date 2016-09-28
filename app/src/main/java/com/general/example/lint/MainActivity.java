78ddb5dcb44e0179e0b9f548ffe60eaebf3441d5
renwenjie <ren_wj@163.com>
2016-09-28 17:08:35 +0800

    使用最新的方式来上传Bintray

--- app/src/main/java/com/general/example/lint/MainActivity.java
+++ app/src/main/java/com/general/example/lint/MainActivity.java
@@ -9,8 +9,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
 
-        Log.d("", "");
+        //Log.d("", "");
     }
 }
